ae2a23b7340645e5ac8aab2d5f93cc72c606619a
VanHuy131 <nguyenhuy.hn723@gmail.com>
2023-03-09 16:19:54 +0700

    Update GameData.cs

--- Assets/script/GameData.cs
+++ Assets/script/GameData.cs
@@ -1,10 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class GameData : MonoBehaviour
 {
     public int levelPassed = 0;
     public int cLevel = 0;
-    
+    public static string lastwindow = "";
+    public static int totalLevel = 10;
+    public static GameData instance;
+    public static GameData getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new GameData();
+        }
+        return instance;
+    }
+    name = 
+    public void lockGame(bool _lock)
+    {
+        islock = _lock;
+       
+    }
+    public bool isLock
+    {
+        get
+        {
+            return isLock;
+        }
+    }
 }
