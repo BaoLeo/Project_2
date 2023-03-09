533b935497e93a17ef5e57f7b349bce89d4e1b58
Nguyen Phuc Bao Nguyen <nguyennpb.1808@gmail.com>
2023-03-09 16:04:13 +0700

    up

---
+++ Assets/script/GameData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    public int levelPassed = 0;
+    public int cLevel = 0;
+    
+}
---
+++ Assets/script/Level Script/Level1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1 : MonoBehaviour
+{
+    [SerializeField] GameObject toy_pig;
+    void TapToyPig()
+    {
+        
+    }    
+}
